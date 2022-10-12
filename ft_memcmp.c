0e77d48218d3ea39c2c4590f5c3441dc10a58f36
Abdoul <abdo@Pro.local>
2022-10-12 16:30:35 +0100

    a small change

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -15,8 +15,8 @@
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t			i;
-	unsigned char 	s_1;
-	unsigned char	s_2;
+	unsigned char 	*s_1;
+	unsigned char	*s_2;
 
 	i = 0;
 	s_1 = (unsigned char *)s1;
