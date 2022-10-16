c684c16f634407dc4c6ac9eb1197e3289cda01f5
Abderrahman Elkhalil <aelkhali@e2r3p5.1337.ma>
2022-10-16 16:19:06 +0100

    new algo for the split function still norminette

--- ft_split.c
+++ ft_split.c
@@ -6,88 +6,67 @@
 /*   By: aelkhali <aelkhali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 14:19:09 by aelkhali          #+#    #+#             */
-/*   Updated: 2022/10/16 13:41:08 by aelkhali         ###   ########.fr       */
+/*   Updated: 2022/10/16 16:17:53 by aelkhali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	wd_count(char const *s, char c)
+static int	wd_counter(char const *str, char c)
 {
 	int	i;
-	int	wd_c;
+	int	num_wd;
 
 	i = 0;
-	wd_c = 0;
-	while (s[i]) 
+	num_wd = 0;
+	while (str[i])
 	{
-		while (s[i] && s[i] == c)
+		while (str[i] && str[i] == c)
 			i++;
-		if (s[i] && s[i] != c)
+		if (str[i] && str[i] != c)
 		{
-			wd_c++;
-			while (s[i] && s[i] != c)
+			num_wd++;
+			while (str[i] && str[i] != c)
 				i++;
 		}
 	}
-	return (wd_c);
+	return (num_wd);
 }
 
-int copy_wd(char c, char const  *str, int len)
-{
-	int coun = 0;
-	while (str[len] != c)
-	{
-		len++;
-		coun ++;
-	}
-	return(coun);
-}
 char	**ft_split(char const *s, char c)
 {
-	char	**splited_strs;
+	char	**spl_strs;
+	int		retu_count;
+	int		wd_len;
 	int		i;
 	int		j;
-	int		sub_len;
-	int		m;
 
 	i = 0;
 	j = 0;
-	sub_len = 0;
-	m = 0;
-	splited_strs = malloc(sizeof(char *) * wd_count(s, c) + 1);
-	if (!splited_strs)
-		return(NULL);
+	wd_len = 0;
+	if (!s)
+		return (NULL);
+	retu_count = wd_counter(s, c);
+	spl_strs = malloc(sizeof(char **) * retu_count);
+	if (!spl_strs)
+		return (NULL);
+	spl_strs[retu_count] = NULL;
 	while (s[i])
 	{
 		while (s[i] && s[i] == c)
 			i++;
 		if (s[i] && s[i] != c)
 		{
-			splited_strs[j] = malloc(copy_wd(c, s,i));
-			while (s[i] != c)
+			while (s[i] && s[i] != c)
 			{
-				splited_strs[j][m] = s[i];
-				i ++;
-				m ++;
+				wd_len++;
+				i++;
 			}
-			splited_strs[j][m] = 0;
-			m = 0;
+			spl_strs[j] = malloc(wd_len  + 1);
+			ft_strlcpy(spl_strs[j], s + i - wd_len, wd_len + 1);
+			wd_len = '\0';
 			j++;
 		}
 	}
-	splited_strs[j] = NULL;
-	return(splited_strs);
-}
-
-#include <stdio.h>
-
-int main(int ac, char **av)
-{
-	(void )ac;
-	int count = wd_count(av[1],av[2][0]);
-	char **lol = ft_split(av[1],av[2][0]);
-	for (int i = 0; i < count; i++)
-		puts(lol[i]);
-	return (0);
+	return (spl_strs);
 }
